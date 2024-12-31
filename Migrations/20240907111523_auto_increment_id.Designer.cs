625f05a7a287c4aad6da75062895cf06111e16e5
notobo15 <binh15122003@gmail.com>
2024-12-31 23:33:54 +0700

    added: mirations folder

--- Migrations/20240907111523_auto_increment_id.Designer.cs
+++ Migrations/20240907111523_auto_increment_id.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RecruitmentApp.Models;
 
 namespace RecruitmentApp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240907111523_auto_increment_id")]
+    partial class auto_increment_id
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -530,6 +532,9 @@ namespace RecruitmentApp.Migrations
                         .ValueGeneratedOnAdd()
                         .HasColumnType("int")
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+                    b.Property<int?>("AddId")
+                        .HasColumnType("int");
 
                     b.Property<int?>("AddressId")
                         .HasColumnType("int");
---
+++ Migrations/20240907135240_post_address.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecruitmentApp.Migrations
+{
+    public partial class post_address : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddId",
+                table: "Posts");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AddId",
+                table: "Posts",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
