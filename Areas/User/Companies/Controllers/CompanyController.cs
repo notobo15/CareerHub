4f98af8f899274798cc0dd2b22e639a73aee5287
notobo15 <binh15122003@gmail.com>
2025-02-14 09:30:45 +0700

    added: implement CompanyController with basic routing and view methods

---
+++ Areas/User/Companies/Controllers/CompanyController.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RecruitmentApp.Models;
+
+namespace RecruitmentApp.Areas.User.Companies.Controllers
+{
+  [Area("User/Companies")]
+
+  [Route("/companies")]
+  [Route("/nha-tuyen-dung")]
+  public class CompanyController : Controller
+  {
+    private readonly AppDbContext _context;
+
+    public CompanyController(AppDbContext context)
+    {
+      _context = context;
+    }
+
+    [HttpGet]
+    [Route("")]
+    public IActionResult Index()
+    {
+        return View();
+
+    }
+
+
+        [HttpGet]
+        [Route("{name}")]
+        public IActionResult Detail(string name)
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        [Route("{slug}/review")]
+        public IActionResult Review(string slug)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("{slug}/review/new")]
+        public IActionResult New(string slug)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("{slug}/review/new")]
+        public IActionResult New(string slug, string model)
+        {
+            return View();
+        }
+    }
+}
