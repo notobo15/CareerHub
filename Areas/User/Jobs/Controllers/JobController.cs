f165cd4fe76b7c5da42d640a3018e2a55185e9f9
notobo15 <binh15122003@gmail.com>
2025-02-14 09:31:57 +0700

    added: implement Profile and Job controllers with routing and views, and create layout and view start files

---
+++ Areas/User/Jobs/Controllers/JobController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecruitmentApp.Areas.User.Jobs.Controllers
+{
+    [Area("User/Jobs")]
+
+    [Route("/jobs")]
+    [Route("/viec-lam")]
+    public class ProfileController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Areas/User/Profiles/Controllers/ProfileController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecruitmentApp.Areas.User.Profiles.Controllers
+{
+    [Area("User/Profiles")]
+
+    [Route("/profile")]
+    [Route("/ho-so")]
+    public class ProfileController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        [Route("dashboard")]
+        [Route("thong-ke")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("manage-cv")]
+        public IActionResult ManageCV()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("profile-cv")]
+        public IActionResult ProfileCV()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("my-jobs")]
+        public IActionResult MyJobs()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("my-jobs/applied")]
+        public IActionResult MyJobsApplied()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("my-jobs/recent-viewed")]
+        public IActionResult MyJobsRecentViewed()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        [Route("job-invitation")]
+        public IActionResult JobInvitation()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        [Route("settings")]
+        public IActionResult Settings()
+        {
+            return View();
+        }
+    }
+}
