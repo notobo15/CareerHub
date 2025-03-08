d806548bbf857b670f40df4019388ffe838b0a53
Nguyen Thanh Binh <binh15122003@gmail.com>
2025-03-08 22:15:47 +0700

    added: implement ListFormatter and TimeFormatter utilities; refactor GenerateSlug method for clarity

---
+++ Utilities/ListFormatter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RecruitmentApp.Utilities
+{
+    public class ListFormatter
+    {
+        public static List<List<T>> SplitList<T>(List<T> source, int chunkSize)
+        {
+            return source
+                .Select((item, index) => new { item, index })
+                .GroupBy(x => x.index / chunkSize)
+                .Select(group => group.Select(x => x.item).ToList())
+                .ToList();
+        }
+    }
+}
---
+++ Utilities/TimeFormatter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RecruitmentApp.Utilities
+{
+    public static class TimeFormatter
+    {
+        public static string GetTimeAgo(DateTime pastTime)
+        {
+            TimeSpan timeDifference = DateTime.Now - pastTime;
+
+            if (timeDifference.TotalMinutes < 60)
+            {
+                return $"{(int)timeDifference.TotalMinutes} phút trước";
+            }
+            else if (timeDifference.TotalHours < 24)
+            {
+                return $"{(int)timeDifference.TotalHours} giờ trước";
+            }
+            else
+            {
+                int days = (int)timeDifference.TotalDays;
+                int hours = (int)(timeDifference.TotalHours % 24);
+                return $"{days} ngày {hours} giờ trước";
+            }
+
+        }
+    }
+}
