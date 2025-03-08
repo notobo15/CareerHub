64a7138ce574162bc262b8a2cef461cf4a6b217b
Nguyen Thanh Binh <binh15122003@gmail.com>
2025-03-08 22:22:28 +0700

    added: implement JobDetailViewComponent to display post details

---
+++ Components/JobDetailViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecruitmentApp.DTOs;
+
+namespace RecruitmentApp.Components
+{
+    public class JobDetailViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(PostDTO post)
+        {
+            return View(post);
+        }
+    }
+}
