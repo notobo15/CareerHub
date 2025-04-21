c535ffaa96dfb4077d583a23dfc437dc7bf7a1fe
notobo15 <binh15122003@gmail.com>
2025-04-21 17:05:27 +0700

    added: Hub class

---
+++ Hubs/MailHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace RecruitmentApp.Hubs
+{
+    public class MailHub : Hub
+    {
+        // Gửi thông báo đến user cụ thể (theo userId)
+        public async Task NotifyNewMail(int userId)
+        {
+            await Clients.User(userId.ToString()).SendAsync("ReceiveMailNotification");
+        }
+    }
+}
