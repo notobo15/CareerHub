e8d6bc4f9dc94c7ce3f270bbdcbf8539b6569b5e
notobo15 <binh15122003@gmail.com>
2025-04-21 17:14:23 +0700

    added: Components

---
+++ Components/FilterViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecruitmentApp.DTOs;
+using RecruitmentApp.ModelViews;
+
+namespace RecruitmentApp.Components
+{
+    public class FilterViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(FilterDataComponent filterData)
+        {
+            return View(filterData);
+        }
+    }
+}
