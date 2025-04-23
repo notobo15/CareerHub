033c559b79f646595a6daf6668232cd1f9540383
Nguyen Thanh Binh <binh15122003@gmail.com>
2025-04-23 16:26:51 +0700

    added: blogs management

---
+++ Areas/Admin/Blogs/Controllers/BlogController.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RecruitmentApp.Models;
+
+namespace RecruitmentApp.Areas.Admin.Blogs.Controllers
+{
+    [Area("Admin/Blogs")]
+    [Route("/admin/blog/[action]")]
+    public class BlogController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BlogController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Blog
+        public async Task<IActionResult> Index()
+        {
+            ViewData["Title"] = "Quản lý Blog";
+            ViewBag.Message = TempData["SuccessMessage"];
+            return View(await _context.Blogs.Include(b => b.Author).Include(b => b.Category).ToListAsync());
+        }
+
+        // GET: Blog/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            ViewData["Title"] = "Chi tiết Blog";
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blog = await _context.Blogs
+                .Include(b => b.Author)
+                .Include(b => b.Category)
+                .FirstOrDefaultAsync(m => m.BlogId == id);
+
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            return View(blog);
+        }
+
+        // GET: Blog/Create
+        public IActionResult Create()
+        {
+            ViewData["Title"] = "Tạo mới Blog";
+            ViewData["Categories"] = new SelectList(_context.BlogCategories, "CategoryId", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Title,Content,Description,Slug,ThumbnailUrl,CreatedAt,ReadTimeMinutes,IsPublished,AuthorId,CategoryId")] Blog blog)
+        {
+            ViewData["Title"] = "Tạo mới Blog";
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(blog);
+                await _context.SaveChangesAsync();
+                TempData["SuccessMessage"] = $"Tạo mới thành công (ID: {blog.BlogId})!";
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["Categories"] = new SelectList(_context.BlogCategories, "CategoryId", "Name", blog.CategoryId);
+            return View(blog);
+        }
+
+        // GET: Blog/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            ViewData["Title"] = "Chỉnh sửa Blog";
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blog = await _context.Blogs.FindAsync(id);
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["Categories"] = new SelectList(_context.BlogCategories, "CategoryId", "Name", blog.CategoryId);
+            return View(blog);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BlogId,Title,Content,Description,Slug,ThumbnailUrl,CreatedAt,UpdatedAt,ReadTimeMinutes,IsPublished,AuthorId,CategoryId")] Blog blog)
+        {
+            ViewData["Title"] = "Chỉnh sửa Blog";
+
+            if (id != blog.BlogId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    blog.UpdatedAt = DateTime.Now;
+                    _context.Update(blog);
+                    await _context.SaveChangesAsync();
+                    TempData["SuccessMessage"] = $"Cập nhật thành công (ID: {blog.BlogId})!";
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BlogExists(blog.BlogId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            ViewData["Categories"] = new SelectList(_context.BlogCategories, "CategoryId", "Name", blog.CategoryId);
+            return View(blog);
+        }
+
+        // GET: Blog/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            ViewData["Title"] = "Xóa Blog";
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blog = await _context.Blogs
+                .Include(b => b.Author)
+                .Include(b => b.Category)
+                .FirstOrDefaultAsync(m => m.BlogId == id);
+
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            return View(blog);
+        }
+
+        // POST: Blog/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var blog = await _context.Blogs.FindAsync(id);
+            _context.Blogs.Remove(blog);
+            await _context.SaveChangesAsync();
+            TempData["SuccessMessage"] = $"Đã xóa thành công (ID: {id})!";
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BlogExists(int id)
+        {
+            return _context.Blogs.Any(e => e.BlogId == id);
+        }
+    }
+}
